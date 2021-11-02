b5d631f421db7e3b18f7c66aec83e4e9898be706
Jamie-RVA <47825778+jamie-miller-rva@users.noreply.github.com>
2021-11-02 19:59:16 -0400

    adding solved files

---
+++ 03-Lessons-Python/Day_1/02-Ins_Variables/Solved/jamie_variables.py
@@ -0,0 +1,18 @@
+# create a string variable
+title = "Data Analyst"
+first_name = "Jamie"
+
+# create a integer varialbe
+years_experience = 20 
+
+# create a boolean
+expert_status = True
+
+# print name and title
+print(first_name + " is a " + title)
+
+# print using an int
+print("He has been an analyst for " + str(years_experience) )
+
+# print using an f-string
+print(f"Jamie Expert status is: {years_experience} some other stuff")
